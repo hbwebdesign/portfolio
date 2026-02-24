be7662e3e8cb852bbc503d829af9505b21cfd1dc
hemant bramhe <hemant.bramhe@lispl.org>
2026-02-24 15:02:54 +0530

    added social media links in footer

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,10 +4,19 @@ export default function Contact() {
   return (
     <div>
       <section id="contact" className="bg-dark text-white py-3 footer">
-      <div className="container text-center">
-        <p className='m-0 opacity-50'>Copy Right 2026 © By Hemant All Rights Reserved</p>
-      </div>
-    </section>
+        <div className="container text-center">
+          <h6>Social Media Links</h6>
+          <div className="f_s_menu">
+            <a href="https://www.linkedin.com/in/hemant-bramhe-b06427222?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target='_blank' rel="noopener noreferrer"><i className="fab fa-linkedin"></i></a>
+            <a href="https://www.facebook.com/share/19dENWbGc6/" target='_blank' rel="noopener noreferrer"><i class="fa-brands fa-facebook"></i></a>
+
+            <a href="https://www.instagram.com/hemant12344985?utm_source=qr&igsh=MW94a3YzNXI0eWQwOQ==" target='_blank' rel="noopener noreferrer"><i class="fa-brands fa-square-instagram"></i></a>
+
+          </div>
+          <p className='m-0 opacity-50'>Copy Right 2026 © By Hemant All Rights Reserved</p>
+
+        </div>
+      </section>
     </div>
   )
 }
